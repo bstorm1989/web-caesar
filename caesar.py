f46d2e90412184d4dbbbb4067f6b9b39ebdc08a1
Brandon Storm <brandon.storm1989@gmail.com>
2017-01-22 21:43:08 -0600

    initial commit; displays encrypted Hello world message

---
+++ caesar.py
@@ -0,0 +1,28 @@
+def encrypt(text, rot):
+    message = ''
+    for char in text:
+        newchar = rotate_character(char, rot)
+        message = message + newchar
+    return message
+
+def alphabet_position(char):
+    newchar = str.lower(char)
+    CharIndex = alphabet.index(newchar)
+    return CharIndex
+
+
+def rotate_character(char, rot):
+    if char.isalpha() == True:
+        LetterIndex = alphabet_position(char)
+        RotatedIndex = LetterIndex + rot
+        if RotatedIndex < 26:
+            encrypted = alphabet[RotatedIndex]
+        else:
+            encrypted = alphabet[RotatedIndex % 26]
+        if char.isupper() == True:
+            return encrypted.upper()
+        else:
+            return encrypted
+    return char
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
